a23cf263d764863e3a5a6c45c63720fcb1cb06d1
anderson-souz4 <andersondesouzalopes69@gmail.com>
2023-03-07 17:29:56 -0300

    Criação do MS de pedidos e adicionando o Eureka

---
+++ src/main/resources/db/migration/V1__cria_tabela_pedidos.sql
@@ -0,0 +1,6 @@
+CREATE TABLE pedidos
+(
+    id        SERIAL PRIMARY KEY,
+    data_hora TIMESTAMP    NOT NULL,
+    status    VARCHAR(255) NOT NULL
+);
---
+++ src/main/resources/db/migration/V2__cria_tabela_item_pedido.sql
@@ -0,0 +1,12 @@
+CREATE TABLE item_do_pedido
+(
+    id         SERIAL PRIMARY KEY,
+    descricao  VARCHAR(255),
+    quantidade INT    NOT NULL,
+    pedido_id  BIGINT NOT NULL REFERENCES pedidos (id)
+);
+
+
+
+
+
